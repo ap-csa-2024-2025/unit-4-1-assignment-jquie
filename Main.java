85d540530de560f9dc23920348c43316d2835732
jquie <jacquelynphan03@gmail.com>
2024-12-17 18:17:49 +0000

    not done

--- Main.java
+++ Main.java
@@ -4,6 +4,42 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    // Problem 1
+    Scanner sc = new Scanner(System.in);
+    /*
+    System.out.println("Enter any numbers (Enter -1 to stop)");
+    int num = 0;
+    int sum = 0;
+    while (num != -1)
+    {
+      num = sc.nextInt();
+      sum += num;
+    }
+    sum++;
+    System.out.println("Sum is " + sum);
+  
+    // Problem 2
+    int max = Integer.MIN_VALUE;
+    System.out.println("How many numbers would you like to input?");
+    int count = sc.nextInt();
+    System.out.println("Enter the scores:");
+    int i = 0;
+    while (i < count)
+    {
+      int input = sc.nextInt();
+      if (input > max)
+      {
+        max = input;
+      }
+      i++;
+    }
+    System.out.println("The largest score is " + max);
+    */
+
+   // Problem 3
+   System.out.println("Input a word");
+   String word = sc.nextLine();
+   int count = word.length();
+   int i = 0;
   }
 }
